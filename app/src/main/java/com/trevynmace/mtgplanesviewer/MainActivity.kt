ece77b78c365412634c146ba35b1b75abb3b9ce1
Trevyn Mace <trevynmace@gmail.com>
2021-04-28 21:59:15 -0600

    adding initial activity layout

--- app/src/main/java/com/trevynmace/mtgplanesviewer/MainActivity.kt
+++ app/src/main/java/com/trevynmace/mtgplanesviewer/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun changeText(text: String) {
-        val textView = findViewById<TextView>(R.id.text)
-        textView.text = text
+//        val textView = findViewById<TextView>(R.id.text)
+//        textView.text = text
     }
 }
